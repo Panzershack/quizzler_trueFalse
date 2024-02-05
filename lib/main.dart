528036649ba7da52bcd0fab508e69949f3a0b41f
Panzershack <pasannim@yahoo.com>
2024-02-05 19:35:50 +0530

    created dynamically changing questions and added a new questions class externally from the main.dart file to handle question objects. had to refer to documentation to understand difference between nullable and non nullable variables (String? cant be null  and String can be null)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -34,16 +35,17 @@ class _QuizPageState extends State<QuizPage> {
     ),
   );
 
-  List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
+  List<Icon> scoreKeeper = [];
+
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
+
+  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -57,7 +59,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionBank[questionNumber].questionText ?? "",
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -82,10 +84,13 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  scoreKeeper.add(Icon(
-                    Icons.check,
-                    color: Colors.green,
-                  ));
+                  bool? correctAns = questionBank[questionNumber].answerText;
+                  if (correctAns == true) {
+                    print('user is correct');
+                  } else {
+                    print('user is wrong');
+                  }
+                  questionNumber++;
                 });
               },
             ),
@@ -106,10 +111,13 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  scoreKeeper.add(Icon(
-                    Icons.close,
-                    color: Colors.red,
-                  ));
+                  bool? correctAns = questionBank[questionNumber].answerText;
+                  if (correctAns == false) {
+                    print('user is correct');
+                  } else {
+                    print('user is wrong');
+                  }
+                  questionNumber++;
                 });
               },
             ),
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String? questionText;
+  bool? answerText;
+
+  Question({required String q, required bool a}) {
+    questionText = q;
+    answerText = a;
+  }
+}
